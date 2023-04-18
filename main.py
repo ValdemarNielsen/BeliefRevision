f17e562606580bfcf1f53b004536bf5f6bedb352
Christian H <christian@hyltoft.net>
2023-04-18 09:36:22 +0200

    knowledgebase append list

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 from typing import List
 
 from sympy import *
+
 x, y = symbols('x,y')
 
 class BeliefRevision():
-    def __init__(self, master=None):
-
-        print("hello")
-
+    def __init__(self):
+        self.knowledgeBase = []
+        self.beliefBase = []
 
 
     def check_logic(self):
+        var = input("Enter a variable: ")
+        value = input("Enter a value: ")
         print("hey")
 
     def beliefBase(self):
         print("hello")
 
-
 #Knowledge Base: P, Q, R and more
 #Define P, Q, R and their values and "beliefs"
 #Update the rest fo the knowledge base based on the new values and revision
 #Start of by an empty knowledge base which is updated with the new values based on user input
-
-    def knowledgeBase(self) -> List:
+    def knowledgebase(self) -> List:
         knowledgeBase = []
-        knowledgeBase.append("P")
-        knowledgeBase.append("Q")
-        knowledgeBase.append("R")
+        var = input("Enter a variable: ")
+        value = input("Enter a value: ")
+        knowledgeBase.append(var)
+        knowledgeBase.append(value)
+
+        if self.check_logic(self, var, value):
+            print("The variable is true")
+
         return knowledgeBase
+
+#Run the program
+if __name__ == '__main__':
+    BeliefRevision().knowledgebase()
