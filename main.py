1c476f313e5c47fbbe2660809a27ed1e81f7a4a8
Valdemar Nielsen <v.nielsen@live.dk>
2023-04-18 09:52:10 +0200

    removed self and init

--- main.py
+++ main.py
@@ -3,29 +3,23 @@ from sympy import *
 
 x, y = symbols('x,y')
 
-class BeliefRevision():
-    def init(self):
-        self.knowledgeBase = []
-        self.beliefBase = []
+def check_logic(var, value):
+    # assume var is always true for now
+    return True
 
-    def check_logic(self, var, value):
-        # assume var is always true for now
-        return True
+def belief_base():
+    print("hello")
 
-    def belief_base(self):
-        print("hello")
+def knowledge_base() -> List:
+    knowledgeBase = []
+    var = input("Enter a variable: ")
+    value = input("Enter a value: ")
+    knowledgeBase.append(var)
+    knowledgeBase.append(value)
 
-    def knowledge_base(self) -> List:
-        knowledgeBase = []
-        var = input("Enter a variable: ")
-        value = input("Enter a value: ")
-        knowledgeBase.append(var)
-        knowledgeBase.append(value)
+    if check_logic(var, value):
+        print("The variable is true")
 
-        if self.check_logic(var, value):
-            print("The variable is true")
+    return knowledgeBase
 
-        return knowledgeBase
-
-if __name__ == '__main__':
-    BeliefRevision().knowledge_base()
+knowledge_base()
