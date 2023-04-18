19ba911f4c08e0f31fe0196eccbfc8b59fb164e9
Valdemar Nielsen <v.nielsen@live.dk>
2023-04-18 09:49:30 +0200

    starting over

--- main.py
+++ main.py
@@ -3,25 +3,32 @@ from typing import List
 from sympy import *
 from sympy.logic import POSform
 
-p, q, s, t = symbols('p, q, s, t')
+p, q, r, s, t = symbols('p, q, r, s, t')
+
 
 def check_logic():
     print("hey")
 
+
 def beliefBase():
     print("hello")
 
-def knowledgeBase():
-    print("howdy")
 
-#Knowledge Base: P, Q, R and more
-#Define P, Q, R and their values and "beliefs"
-#Update the rest fo the knowledge base based on the new values and revision
-#Start of by an empty knowledge base which is updated with the new values based on user input
+# Knowledge Base: p, q, r and more
+# Define p, q, r and their values and "beliefs"
+# Update the rest fo the knowledge base based on the new values and revision
+# Start of by an empty knowledge base which is updated with the new values based on user input
 
 def knowledgeBase() -> List:
+    input1 = input()
+    print(input1)
     knowledgeBase = []
-    knowledgeBase.append("P")
-    knowledgeBase.append("Q")
-    knowledgeBase.append("R")
+    knowledgeBase.append("p")
+    knowledgeBase.append("q")
+    knowledgeBase.append("r")
     return knowledgeBase
+
+def runitall():
+    print("hello")
+
+knowledgeBase()
