0c3476d0b6ebdebe58bb4e1448291ea29d24a07f
Valdemar Nielsen <v.nielsen@live.dk>
2023-04-18 08:33:15 +0200

    setting up

---
+++ beliefBase.py
@@ -0,0 +1,8 @@
+
+
+#
+class beliefBase():
+
+    print("hello")
+
+
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import sympy
+
+
 class beliefRevision():
     def __init__(self, master=None):
         print("hello")
 
+    def check_logic(self):
+        print("hey")
+
+    def beliefBase(self):
+        print("hello")
