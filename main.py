828ade676256cb489d8f4a91ffa511fccb24c88b
Christian H <christian@hyltoft.net>
2023-04-18 08:59:32 +0200

    main file and widgets

--- main.py
+++ main.py
@@ -2,15 +2,43 @@ from sympy import *
 
 x, y = symbols('x,y')
 
-class beliefRevision():
+class BeliefRevision():
     def __init__(self, master=None):
 
         print("hello")
+        self.master = master
+        self.master.title("Belief Revision")
+        self.master.geometry("500x500")
+        self.master.resizable(0, 0)
+        self.master.configure(background='white')
+        self.master.grid_rowconfigure(0, weight=1)
+        self.master.grid_columnconfigure(0, weight=1)
+        self.frame = Frame(self.master, background='white')
+        self.frame.grid(row=0, column=0, sticky="nsew")
+        self.frame.grid_rowconfigure(0, weight=1)
+        self.frame.grid_columnconfigure(0, weight=1)
+        self.frame.grid_propagate(False)
+        self.createWidgets()
+
+
 
     def check_logic(self):
         print("hey")
 
     def beliefBase(self):
         print("hello")
+
+    def createWidgets(self):
+        self.button = Button(self.frame, text="Check Logic", command=self.check_logic)
+        self.button.grid(row=0, column=0)
+        self.button = Button(self.frame, text="Belief Base", command=self.beliefBase)
+        self.button.grid(row=0, column=1)
+
+if __name__ == "__main__":
+    root = Tk()
+    app = BeliefRevision(root)
+    root.mainloop()
+
+
 
 
