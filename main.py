5ff71a169c6e4cf07b7bae02ed3ebed52de90ae1
Christian H <christian@hyltoft.net>
2023-04-18 09:42:49 +0200

    taking user input as variable and value

--- main.py
+++ main.py
@@ -1,39 +1,31 @@
 from typing import List
-
 from sympy import *
 
 x, y = symbols('x,y')
 
 class BeliefRevision():
-    def __init__(self):
+    def init(self):
         self.knowledgeBase = []
         self.beliefBase = []
 
+    def check_logic(self, var, value):
+        # assume var is always true for now
+        return True
 
-    def check_logic(self):
-        var = input("Enter a variable: ")
-        value = input("Enter a value: ")
-        print("hey")
-
-    def beliefBase(self):
+    def belief_base(self):
         print("hello")
 
-#Knowledge Base: P, Q, R and more
-#Define P, Q, R and their values and "beliefs"
-#Update the rest fo the knowledge base based on the new values and revision
-#Start of by an empty knowledge base which is updated with the new values based on user input
-    def knowledgebase(self) -> List:
+    def knowledge_base(self) -> List:
         knowledgeBase = []
         var = input("Enter a variable: ")
         value = input("Enter a value: ")
         knowledgeBase.append(var)
         knowledgeBase.append(value)
 
-        if self.check_logic(self, var, value):
+        if self.check_logic(var, value):
             print("The variable is true")
 
         return knowledgeBase
 
-#Run the program
 if __name__ == '__main__':
-    BeliefRevision().knowledgebase()
+    BeliefRevision().knowledge_base()
