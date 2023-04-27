3c0198ffc1856f0dd182de5dd7c809be76895f99
Christian H <christian@hyltoft.net>
2023-04-27 08:33:43 +0200

    taking user input as variable and value

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
-from typing import List
 from sympy import *
+from sympy.logic.boolalg import to_cnf, Not, And, Or
 
-x, y = symbols('x,y')
-
-def check_logic(var, value):
-    # assume var is always true for now
-    return True
 
 def belief_base():
-    print("hello")
+    # Initialize empty beliefs in an array
+    beliefs = []
 
-def knowledge_base() -> List:
-    knowledgeBase = []
-    var = input("Enter a variable: ")
-    value = input("Enter a value: ")
-    knowledgeBase.append(var)
-    knowledgeBase.append(value)
+    # Infinite loop to keep prompting the user to insert new beliefs to make belief revision.
+    while True:
+        # Prompt the user to enter a belief
+        belief_str = input("Enter a belief (or 'done' to finish): ")
 
-    if check_logic(var, value):
-        print("The variable is true")
+        # If the user types "done" in console the program will stop running
+        if belief_str == "done":
+            break
 
-    return knowledgeBase
+        # Parse the belief expression into a sympy expression
+        belief = sympify(belief_str)
 
-knowledge_base()
+        # Checking for contradictions in the belief base and swap the conflicting beliefs with the new belief
+        conflicting_beliefs = []
+        for c in beliefs:
+            if simplify(belief & c) == false:
+                conflicting_beliefs.append(c)
+        if conflicting_beliefs:
+            print("Contradiction found. The new belief contradicts the following beliefbase:", conflicting_beliefs)
+            beliefs = [c if c not in conflicting_beliefs else belief for c in beliefs]
+            continue
+
+        # This condition will check if there are any duplicate beliefs in the belief base
+        if belief in beliefs:
+            print("Duplicate found. Please try again.")
+            continue
+
+        # Add the belief to the list of beliefs
+        beliefs.append(belief)
+
+        # Print newest belief
+        print("Current beliefs:", beliefs)
+
+    # Print the final CNF in belief form
+    cnf = simplify(And(*beliefs))
+    beliefs = list(cnf.args)
+    print("The final CNF for belief base:", beliefs)
+
+
+if __name__ == "__main__":
+    belief_base()
